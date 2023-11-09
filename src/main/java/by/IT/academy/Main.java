a58d94209ef761458bd61c07934f95fd219ff1ad
Anzhelika <likapiv436587@gmail.com>
2023-11-09 23:31:41 +0300

    Task1

--- src/main/java/by/IT/academy/Main.java
+++ src/main/java/by/IT/academy/Main.java
@@ -6,5 +6,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.printf("Поставьте зачет))");
         System.out.printf("Поставьте зачет, пожалуйста))");
+        System.out.printf("Поставьте зачет, пожалуйста)))))");
+
     }
 }
